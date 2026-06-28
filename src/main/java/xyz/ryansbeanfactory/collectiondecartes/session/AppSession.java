d5a1d038acbebfd21368653f2c59a5fdc1331866
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2026-06-28 19:52:21 +0300

    updated AppSession with theme details

--- src/main/java/xyz/ryansbeanfactory/collectiondecartes/session/AppSession.java
+++ src/main/java/xyz/ryansbeanfactory/collectiondecartes/session/AppSession.java
@@ -1,26 +1,43 @@
 package xyz.ryansbeanfactory.collectiondecartes.session;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import xyz.ryansbeanfactory.collectiondecartes.theme.Theme;
+
 public class AppSession {
 
     private static AppSession instance;
+
     private DeckLanguage deckLanguage = DeckLanguage.FRENCH;
+    private final ObjectProperty<Theme> theme = new SimpleObjectProperty<>(Theme.DEFAULT);
 
-    public AppSession() {
-        instance = AppSession.getInstance();
-    }
+    private AppSession() {}
 
-    public static AppSession getInstance() {
-        return instance = instance == null ?
-                new AppSession()
-                : instance;
+    public static synchronized AppSession getInstance() {
+        if (instance == null) {
+            instance = new AppSession();
+        }
+        return instance;
     }
 
     public void setDeckLanguage(DeckLanguage deckLanguage) {
         this.deckLanguage = deckLanguage;
+        System.out.println("DeckLanguage: " + this.deckLanguage);
     }
 
     public DeckLanguage getDeckLanguage() {
         return deckLanguage;
     }
 
+    public void setTheme(Theme theme) {
+        this.theme.set(theme);
+    }
+
+    public Theme getTheme() {
+        return theme.get();
+    }
+
+    public ObjectProperty<Theme> themeProperty() {
+        return theme;
+    }
 }
