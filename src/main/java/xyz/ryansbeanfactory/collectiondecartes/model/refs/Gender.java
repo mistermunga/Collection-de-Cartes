3eb402542eeb8d3acb35b6581a771aa3749e5d5c
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2026-06-28 19:50:36 +0300

    enums for words

---
+++ src/main/java/xyz/ryansbeanfactory/collectiondecartes/model/refs/Gender.java
@@ -0,0 +1,5 @@
+package xyz.ryansbeanfactory.collectiondecartes.model.refs;
+
+public enum Gender {
+    MASCULINE, FEMININE, NEUTER, NONE
+}
---
+++ src/main/java/xyz/ryansbeanfactory/collectiondecartes/model/refs/PartOfSpeech.java
@@ -0,0 +1,5 @@
+package xyz.ryansbeanfactory.collectiondecartes.model.refs;
+
+public enum PartOfSpeech {
+    NOUN, VERB, ADJECTIVE, ADVERB, PREPOSITION, CONJUNCTION, PRONOUN, INTERJECTION
+}
