dd9cdd358bbf5424b888eeb5f995a48d868e9e23
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2026-06-28 19:54:20 +0300

    integrated language init

--- src/main/java/xyz/ryansbeanfactory/collectiondecartes/CarteApplication.java
+++ src/main/java/xyz/ryansbeanfactory/collectiondecartes/CarteApplication.java
@@ -2,6 +2,9 @@ package xyz.ryansbeanfactory.collectiondecartes;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import xyz.ryansbeanfactory.collectiondecartes.database.DatabaseManager;
+
+import java.util.List;
 
 public class CarteApplication extends Application {
 
@@ -20,8 +23,19 @@ public class CarteApplication extends Application {
     public void start(Stage primaryStage) throws Exception {
         instance = this;
         this.primaryStage = primaryStage;
+        List<String> databases = initializeDatabases();
 
-        SceneManager.getInstance().showLanding();
+        SceneManager.getInstance().showLanding(databases);
+    }
+
+    private List<String> initializeDatabases() throws Exception {
+        try {
+            DatabaseManager databaseManager = new DatabaseManager();
+            return databaseManager.getLanguages();
+        } catch (Exception e) {
+            System.err.println("Database initialization failed \n" + e.getMessage());
+            throw new Exception();
+        }
     }
 
     public Stage getPrimaryStage() {
