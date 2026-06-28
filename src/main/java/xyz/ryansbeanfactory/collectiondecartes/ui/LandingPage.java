06b7e0e02036c4857645c461191eea79a803cc54
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2026-06-28 19:54:35 +0300

    integrated language init

--- src/main/java/xyz/ryansbeanfactory/collectiondecartes/ui/LandingPage.java
+++ src/main/java/xyz/ryansbeanfactory/collectiondecartes/ui/LandingPage.java
@@ -1,14 +1,44 @@
 package xyz.ryansbeanfactory.collectiondecartes.ui;
 
-import javafx.scene.control.Label;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import xyz.ryansbeanfactory.collectiondecartes.SceneManager;
+import xyz.ryansbeanfactory.collectiondecartes.theme.ThemeManager;
+import xyz.ryansbeanfactory.collectiondecartes.ui.components.LargeButton;
+
+import java.util.List;
 
 public class LandingPage extends VBox {
 
-    public LandingPage() {
-        Label text = new Label();
-        text.setText("Welcome to Cartes");
-        this.getChildren().add(text);
+    private final List<String> languages;
+
+    public LandingPage(List<String> languages) {
+        this.languages = languages;
+
+        ThemeManager.getInstance().registerComponent(this);
+        this.getStyleClass().add("app-background");
+
+        this.setAlignment(Pos.CENTER);
+        this.setSpacing(40);
+        this.setPadding(new Insets(60, 40, 60, 40));
+
+        showLanguageOptions();
+
+    }
+
+    private void showLanguageOptions() {
+        HBox container = new HBox();
+        for (String language : languages) {
+            LargeButton button = new LargeButton(
+                    language,
+                    () -> SceneManager.getInstance().showCrossRoads(language)
+            );
+            container.getChildren().add(button);
+        }
+        container.setAlignment(Pos.CENTER);
+        this.getChildren().add(container);
     }
 
 }
