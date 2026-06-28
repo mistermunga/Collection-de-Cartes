c058c3300fd1a0362395474183abb10353c8c0ed
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2026-06-28 19:49:20 +0300

    ide files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,12 @@
 module xyz.ryansbeanfactory.collectiondecartes {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens xyz.ryansbeanfactory.collectiondecartes to javafx.fxml;
     exports xyz.ryansbeanfactory.collectiondecartes;
     exports xyz.ryansbeanfactory.collectiondecartes.session;
+    exports xyz.ryansbeanfactory.collectiondecartes.theme;
     opens xyz.ryansbeanfactory.collectiondecartes.session to javafx.fxml;
 }
