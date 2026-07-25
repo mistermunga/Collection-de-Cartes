a0f5f2d94b9df4969597c26044b22c5ad8f175f0
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2026-07-25 15:00:01 +0300

    button element

--- src/main/java/xyz/ryansbeanfactory/collectiondecartes/ui/components/LargeButton.java
+++ src/main/java/xyz/ryansbeanfactory/collectiondecartes/ui/components/LargeButton.java
@@ -4,28 +4,27 @@ import javafx.scene.control.Button;
 import xyz.ryansbeanfactory.collectiondecartes.theme.ThemeManager;
 
 import java.util.function.Consumer;
-import java.util.function.Function;
 
 public class LargeButton extends Button {
 
     public LargeButton() {
-        ThemeManager.getInstance().registerComponent(this);
-        this.getStyleClass().add("element");
+        initialize();
     }
 
     public LargeButton(String text, Runnable onClick) {
         super(text);
-        ThemeManager.getInstance().registerComponent(this);
-        this.getStyleClass().add("element");
-
+        initialize();
         this.setOnAction(_ -> onClick.run());
     }
 
     public LargeButton(String text, Consumer<String> onClick) {
         super(text);
+        initialize();
+        this.setOnAction(_ -> onClick.accept(getText()));
+    }
+
+    private void initialize() {
         ThemeManager.getInstance().registerComponent(this);
         this.getStyleClass().add("element");
-
-        this.setOnAction(_ -> onClick.accept(text));
     }
 }
