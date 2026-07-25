bb861fa7584b39e2b7549e911f7f76431ff24772
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2026-07-25 19:30:18 +0300

    variable app name

--- src/main/java/xyz/ryansbeanfactory/collectiondecartes/session/DeckLanguage.java
+++ src/main/java/xyz/ryansbeanfactory/collectiondecartes/session/DeckLanguage.java
@@ -1,6 +1,17 @@
 package xyz.ryansbeanfactory.collectiondecartes.session;
 
 public enum DeckLanguage {
-    FRENCH,
-    ENGLISH
+    FRENCH("Collection de cartes"),
+    ENGLISH("Card Collection"),
+    SPANISH("Colección de cartas");
+
+    private final String appName;
+
+    DeckLanguage(String appName) {
+        this.appName = appName;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
 }
