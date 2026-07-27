fbd9d0c09e8fee27d131b08fc92833a8260bcee1
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2026-07-27 19:16:56 +0300

    redesigned the extensible button

--- src/main/java/xyz/ryansbeanfactory/collectiondecartes/ui/crossroads/components/ExtensibleButton.java
+++ src/main/java/xyz/ryansbeanfactory/collectiondecartes/ui/crossroads/components/ExtensibleButton.java
@@ -3,38 +3,67 @@ package xyz.ryansbeanfactory.collectiondecartes.ui.crossroads.components;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class ExtensibleButton extends VBox {
 
-    private final Map<String, Runnable> options;
     private final Button mainButton;
+    private final List<Button> optionButtons = new ArrayList<>();
+
+    private boolean expanded = false;
 
     public ExtensibleButton(String text, Map<String, Runnable> options) {
-        this.options = options;
-        this.mainButton = new Button(text);
-        this.mainButton.setOnAction(event -> onClick());
-        this.getChildren().add(mainButton);
-    }
 
-    public void onClick() {
-        unSelect();
+        mainButton = new Button(text);
+        mainButton.setOnAction(event -> toggle());
+
+        getChildren().add(mainButton);
+
         for (Map.Entry<String, Runnable> entry : options.entrySet()) {
             Button button = new Button(entry.getKey());
+
+            button.setVisible(false);
+            button.setManaged(false);
+
             button.setOnAction(event -> {
                 entry.getValue().run();
                 collapse();
             });
-            this.getChildren().add(button);
+
+            optionButtons.add(button);
+            getChildren().add(button);
         }
     }
 
-    private void collapse() {
-        this.getChildren().clear();
-        this.getChildren().add(mainButton);
+    private void toggle() {
+        if (expanded) {
+            collapse();
+        } else {
+            expand();
+        }
+    }
+
+    public void expand() {
+        expanded = true;
+
+        for (Button button : optionButtons) {
+            button.setVisible(true);
+            button.setManaged(true);
+        }
+    }
+
+    public void collapse() {
+        expanded = false;
+
+        for (Button button : optionButtons) {
+            button.setVisible(false);
+            button.setManaged(false);
+        }
     }
 
     public void unSelect() {
-        this.getChildren().clear();
+        collapse();
     }
 }
