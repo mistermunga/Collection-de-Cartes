7714d85d70de57e4d3055778308d2ef8270d9ab8
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2026-08-30 23:55:18 +0300

    explicatory comment

--- src/main/java/xyz/ryansbeanfactory/collectiondecartes/ui/crossroads/util/NavOption.java
+++ src/main/java/xyz/ryansbeanfactory/collectiondecartes/ui/crossroads/util/NavOption.java
@@ -1,7 +1,7 @@
 package xyz.ryansbeanfactory.collectiondecartes.ui.crossroads.util;
 
 public enum NavOption {
-    DEFAULT,
+    DEFAULT, // should show stats and misc info
     NEW_WORD,
     NEW_PHRASE,
     FLASHCARDS
