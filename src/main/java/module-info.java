88dd174cd9ce544dde21b420215f11a55fd24287
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2026-08-30 23:54:51 +0300

    exported Gender

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,5 +8,6 @@ module xyz.ryansbeanfactory.collectiondecartes {
     exports xyz.ryansbeanfactory.collectiondecartes;
     exports xyz.ryansbeanfactory.collectiondecartes.session;
     exports xyz.ryansbeanfactory.collectiondecartes.theme;
+    exports xyz.ryansbeanfactory.collectiondecartes.model.refs;
     opens xyz.ryansbeanfactory.collectiondecartes.session to javafx.fxml;
 }
