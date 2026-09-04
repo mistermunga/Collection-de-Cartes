aba81c2f6dd129fe8529680371589c84062828d4
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2026-09-04 15:05:13 +0300

    alignment code

--- src/main/java/xyz/ryansbeanfactory/collectiondecartes/ui/crossroads/sections/pages/AddPhrasePage.java
+++ src/main/java/xyz/ryansbeanfactory/collectiondecartes/ui/crossroads/sections/pages/AddPhrasePage.java
@@ -1,13 +1,15 @@
 package xyz.ryansbeanfactory.collectiondecartes.ui.crossroads.sections.pages;
 
+import javafx.geometry.Pos;
 import javafx.scene.layout.VBox;
 import xyz.ryansbeanfactory.collectiondecartes.ui.components.NewPhraseForm;
 
 public class AddPhrasePage extends VBox {
 
     public AddPhrasePage() {
+        this.setAlignment(Pos.TOP_CENTER);
+
         NewPhraseForm newPhraseForm = new NewPhraseForm();
         this.getChildren().add(newPhraseForm);
     }
-
 }
--- src/main/java/xyz/ryansbeanfactory/collectiondecartes/ui/crossroads/sections/pages/AddWordPage.java
+++ src/main/java/xyz/ryansbeanfactory/collectiondecartes/ui/crossroads/sections/pages/AddWordPage.java
@@ -1,11 +1,14 @@
 package xyz.ryansbeanfactory.collectiondecartes.ui.crossroads.sections.pages;
 
+import javafx.geometry.Pos;
 import javafx.scene.layout.VBox;
 import xyz.ryansbeanfactory.collectiondecartes.ui.components.NewWordForm;
 
 public class AddWordPage extends VBox {
 
     public AddWordPage() {
+        this.setAlignment(Pos.TOP_CENTER);
+
         NewWordForm newWordForm = new NewWordForm();
         this.getChildren().add(newWordForm);
     }
